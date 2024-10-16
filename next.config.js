691c471465fd16bd113f30c6fcec059b76792910
Surya-blippi <suryansh.connect@gmail.com>
2024-10-16 19:36:20 +0530

    Remove webcam functionality and simplify QuestionInput component

--- next.config.js
+++ next.config.js
@@ -8,7 +8,15 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdnjs.cloudflare.com",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://*.firebaseapp.com https://www.gstatic.com;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              font-src 'self' https://fonts.gstatic.com;
+              img-src 'self' data: https:;
+              connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.firebaseapp.com;
+              frame-src 'self' https://*.firebaseapp.com https://*.google.com;
+            `.replace(/\s{2,}/g, ' ').trim()
           },
         ],
       },
